99166564e770e008b417babe46b275f154e7bd33
dmterkula <dmterkula@gmail.com>
2022-08-14 06:52:35 -0400

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by David  Terkula on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
